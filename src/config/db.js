7bc48251d27b638d005c378554cf6df57063bc7f
Jahanara Khatun <zarakhatun58@gmail.com>
2026-04-09 09:56:34 +0530

    update

--- src/config/db.js
+++ src/config/db.js
@@ -4,19 +4,25 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const connectDB = async () => {
   try {
     await pool.query("SELECT 1");
     console.log("✅ PostgreSQL Connected");
-    console.log("DATABASE_URL:", process.env.DATABASE_URL);
+    // console.log("DATABASE_URL:", process.env.DATABASE_URL);
   } catch (err) {
     console.error("❌ PostgreSQL Error", err);
     process.exit(1);
--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,9 @@
 import Redis from 'ioredis';
 
-export const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+export const redis = new Redis(process.env.REDIS_URL, {
+  // host: "127.0.0.1",
+  // port: 6379,
+  enableReadyCheck: false,
   maxRetriesPerRequest: null, // prevent crash
 });
 
